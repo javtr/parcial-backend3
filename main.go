237ebeda45ed44b23cab3eedcb9208902cbef91b
javtr <javtriana.z@gmail.com>
2023-08-24 11:58:15 -0500

    Requerimiento04,main

--- main.go
+++ main.go
@@ -1,27 +1,48 @@
 package main
 
 import (
-	"parcial/internal/tickets"
-	"fmt"
+    "fmt"
+    "parcial/internal/tickets"
 )
 
 func main() {
+    totalTicketsChannel := make(chan int)
+    morningsChannel := make(chan int)
+    averageDestinationChannel := make(chan int)
 
-	// Requerimiento 1
-	total, err := tickets.GetTotalTickets("Brazil")
+    // Requerimiento 1: Obtener el total de tickets para "Brazil"
+    go func() {
+        totalTickets, err := tickets.GetTotalTickets("Brazzczil")
+        if err != nil {
+            fmt.Println("Error en la obtención del total de tickets para Brazil:", err)
+        }
+        totalTicketsChannel <- totalTickets // Enviar el resultado al canal
+    }()
 
-	println("El total de tickets con el pais solicitado es ", total)
-	println(err)
+    // Requerimiento 2: Obtener la cantidad de tickets en el rango horario "earlyMorning"
+    go func() {
+        earlyMorning, err := tickets.GetCountByPeriod("21-22")
+        if err != nil {
+            fmt.Println("Error al obtener la cantidad de tickets en ese rango:", err)
+        }
+        morningsChannel <- earlyMorning // Enviar el resultado al canal
+    }()
 
-	//Requerimiento 2
-	tot2, err2 := tickets.GetCountByPeriod("21-22")
+    // Requerimiento 3: Calcular el promedio de tickets para "Brazil"
+    go func() {
+        averageDestination, err := tickets.AverageDestination("Brazil")
+        if err != nil {
+            fmt.Println("Error al calcular el promedio de tickets para Brazil:", err)
+        }
+        averageDestinationChannel <- averageDestination // Enviar el resultado al canal
+    }()
 
-	println("La cantidad de tickets en ese rango son: ", tot2)
-	println(err2)
+    // Recibir los resultados de los canales
+    totalTicketsResult := <-totalTicketsChannel
+    morningsResult := <-morningsChannel
+    averageDestinationResult := <-averageDestinationChannel
 
-	//Requerimiento 3
-	tot3, err3 := tickets.AverageDestination("Brazil")
-	fmt.Println("El promedio de tickets a ese país es:", tot3, "%")
-	println(err3)
-
+    fmt.Println("El total de tickets con el país solicitado es", totalTicketsResult)
+    fmt.Println("La cantidad de tickets en ese rango es:", morningsResult)
+    fmt.Println("El promedio de tickets a ese país es:", averageDestinationResult, "%")
 }
